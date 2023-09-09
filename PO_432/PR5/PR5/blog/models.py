c0ac3abe0e9fe6ee86bd427f78696ee067cf3523
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-09-09 14:23:58 +0500

    update

---
+++ PO_432/PR5/PR5/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.title
+
