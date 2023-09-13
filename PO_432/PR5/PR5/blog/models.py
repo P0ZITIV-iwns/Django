9eba07b14fc0859c0baa8c01e94afd7d41a8fca2
P0ZITIV <66378765+P0ZITIV-iwns@users.noreply.github.com>
2023-09-13 12:31:25 +0500

    PR7

--- PO_432/PR5/PR5/blog/models.py
+++ PO_432/PR5/PR5/blog/models.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    body = models.TextField()
-    email = models.EmailField(blank=True)
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name="Автор")
+    body = models.TextField(verbose_name="Текст")
+    email = models.EmailField(blank=True, verbose_name="Эл. адрес")
 
     def __str__(self):
         return self.title
